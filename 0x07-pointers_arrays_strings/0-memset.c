191a7c1fcb94af1278d1b06b2130e3c992019c0d
ghkfuture <ganzahenrykevin08@gmail.com>
2026-03-11 04:43:29 -0700

    Cleaned ghost text and fixed logic for task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,21 @@
 #include "main.h"
+
 /**
  * _memset - fills memory with a constant byte
- * @s: memory area to fill
- * @b: constant byte
- * @n: bytes to fill
- * Return: pointer to memory area s
+ * @s: memory area to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-main.h unsigned int i;
+unsigned int i;
 
-main.h for (i = 0; i < n; i++)
-main.h main.h s[i] = b;
-main.h return (s);
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+
+return (s);
 }
