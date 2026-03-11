a9e8e0d5a88a82b37cfe732cab69bda87fe9f6af
ghkfuture <ganzahenrykevin08@gmail.com>
2026-03-11 04:00:50 -0700

    Fix 0-strcat ghosts and add tasks 1 and 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
+ * _strncat - concatenates two strings using at most n bytes from src
  * @dest: destination string
  * @src: source string
+ * @n: number of bytes to use from src
  *
  * Return: pointer to resulting string dest
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
 int j = 0;
@@ -15,7 +16,7 @@ int j = 0;
 while (dest[i] != '\0')
 i++;
 
-while (src[j] != '\0')
+while (j < n && src[j] != '\0')
 {
 dest[i] = src[j];
 i++;
