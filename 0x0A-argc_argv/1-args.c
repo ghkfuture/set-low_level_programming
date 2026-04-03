3ebb33971f6856465dbdab22ca713ddd5db0e4e6
Ganza Henry Kevin <ganzahenrykevin08@gmail.com>
2026-04-03 03:10:20 -0700

    Final 100% green check fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed into it
+ * main - prints the number of arguments passed into it.
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-(void)argv;
 printf("%d\n", argc - 1);
 return (0);
 }
