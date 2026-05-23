63ab6d5ba2e7d722d47786bd207451cade753584
ghkfuture <ghkfuture@gmail.com>
2026-05-23 01:33:11 -0700

    Modularize Floyd logic to satisfy Betty line limits

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,32 +1,68 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list safely (handles loops)
- * @head: pointer to the head node of the list
+ * find_loop_count - Counts unique nodes in a looped listint_t list.
+ * @head: A pointer to the head of the listint_t list.
  *
- * Return: the total number of nodes verified in the list
+ * Return: Number of unique nodes, or 0 if no loop.
+ */
+size_t find_loop_count(const listint_t *head)
+{
+const listint_t *slow = head, *fast = head;
+size_t nodes = 0;
+
+while (slow && fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+{
+slow = head;
+while (slow != fast)
+{
+nodes++;
+slow = slow->next;
+fast = fast->next;
+}
+slow = slow->next;
+while (slow != fast)
+{
+nodes++;
+slow = slow->next;
+}
+return (nodes + 1);
+}
+}
+return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t nodes = 0;
-const listint_t *current = head, *runner;
+size_t nodes, index = 0;
 
-while (current)
+nodes = find_loop_count(head);
+if (nodes == 0)
 {
-printf("[%p] %d\n", (void *)current, current->n);
-nodes++;
-current = current->next;
-runner = head;
+for (; head; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+return (index);
+}
 
-while (runner != current)
+for (index = 0; index < nodes; index++)
 {
-if (runner == current->next)
-{
-printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-return (nodes);
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
 }
-runner = runner->next;
-}
-}
+printf("-> [%p] %d\n", (void *)head, head->n);
+
 return (nodes);
 }
--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,42 @@
 #include "lists.h"
 
 /**
+ * count_loop_nodes - Counts unique nodes in a looped listint_t list.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: Number of unique nodes, or 0 if no loop.
+ */
+size_t count_loop_nodes(listint_t *head)
+{
+listint_t *slow = head, *fast = head;
+size_t nodes = 0;
+
+while (slow && fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+{
+slow = head;
+while (slow != fast)
+{
+nodes++;
+slow = slow->next;
+fast = fast->next;
+}
+slow = slow->next;
+while (slow != fast)
+{
+nodes++;
+slow = slow->next;
+}
+return (nodes + 1);
+}
+}
+return (0);
+}
+
+/**
  * free_listint_safe - Frees a listint_t list safely (handles loops).
  * @h: A double pointer to the head of the listint_t list.
  *
@@ -8,40 +44,32 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-listint_t **list = NULL, **new_list;
-listint_t *next;
-size_t i, count = 0;
+size_t nodes, index = 0;
+listint_t *temp;
 
 if (!h || !*h)
 return (0);
 
+nodes = count_loop_nodes(*h);
+if (nodes == 0)
+{
 while (*h)
 {
-for (i = 0; i < count; i++)
-{
-if (*h == list[i])
-{
-*h = NULL;
-free(list);
-return (count);
-}
-}
-new_list = malloc((count + 1) * sizeof(listint_t *));
-if (!new_list)
-{
-free(list);
-exit(98);
-}
-for (i = 0; i < count; i++)
-new_list[i] = list[i];
-new_list[count] = *h;
-free(list);
-list = new_list;
-next = (*h)->next;
+temp = (*h)->next;
 free(*h);
-*h = next;
-count++;
+*h = temp;
+index++;
 }
-free(list);
-return (count);
+*h = NULL;
+return (index);
+}
+
+for (index = 0; index < nodes; index++)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+}
+*h = NULL;
+return (nodes);
 }
